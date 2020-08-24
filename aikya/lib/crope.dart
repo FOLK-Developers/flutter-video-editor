6fe72303f49b91629a317029f7df015ab375c823
riddhij16 <52314570+riddhij16@users.noreply.github.com>
2020-08-24 13:34:24 +0530

    Add files via upload

---
+++ aikya/lib/crope.dart
@@ -0,0 +1,324 @@
+// import 'dart:ui' as ui;
+// import 'package:app/centered_slider_track_shape.dart';
+import 'package:flutter/material.dart';
+import 'package:crop/crop.dart';
+// import 'package:flutter_icons/flutter_icons.dart';
+// import 'package:url_launcher/url_launcher.dart';
+// import 'package:image_gallery_saver/image_gallery_saver.dart';
+// import 'package:permission_handler/permission_handler.dart';
+
+class Crope extends StatefulWidget {
+  @override
+  _CropeState createState() => _CropeState();
+}
+
+class _CropeState extends State<Crope> {
+  final controller = CropController(aspectRatio: 1000 / 667.0);
+  double _rotation = 0;
+  CropShape shape = CropShape.box;
+
+  void _cropImage() async {
+    final pixelRatio = MediaQuery.of(context).devicePixelRatio;
+    final cropped = await controller.crop(pixelRatio: pixelRatio);
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => Scaffold(
+          appBar: AppBar(
+            title: Text('Crop Result'),
+            centerTitle: true,
+          ),
+          body: Center(
+            child: RawImage(
+              image: cropped,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Crop Demo'),
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            onPressed: _cropImage,
+            icon: Icon(Icons.crop),
+          )
+        ],
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              color: Colors.black,
+              padding: EdgeInsets.all(8),
+              child: Crop(
+                onChanged: (decomposition) {
+                  print(
+                      "Scale : ${decomposition.scale}, Rotation: ${decomposition.rotation}, translation: ${decomposition.translation}");
+                },
+                controller: controller,
+                shape: shape,
+                child: Image.asset(
+                  'assests/me.jpeg',
+                  fit: BoxFit.cover,
+                ),
+                foreground: IgnorePointer(
+                  child: Container(
+                    alignment: Alignment.bottomRight,
+                    child: Text(
+                      'Foreground Object',
+                      style: TextStyle(color: Colors.red),
+                    ),
+                  ),
+                ),
+                helper: shape == CropShape.box
+                    ? Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.white, width: 2),
+                        ),
+                      )
+                    : null,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// void main() => runApp(MyApp());
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Crop Demo',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: MyHomePage(),
+//     );
+//   }
+// }
+
+// class MyHomePage extends StatefulWidget {
+//   @override
+//   _MyHomePageState createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   final controller = CropController(aspectRatio: 1000 / 667.0);
+//   double _rotation = 0;
+//   CropShape shape = CropShape.box;
+
+//   void _cropImage() async {
+//     final pixelRatio = MediaQuery.of(context).devicePixelRatio;
+//     final cropped = await controller.crop(pixelRatio: pixelRatio);
+
+//     Navigator.of(context).push(
+//       MaterialPageRoute(
+//         builder: (context) => Scaffold(
+//           appBar: AppBar(
+//             title: Text('Crop Result'),
+//             centerTitle: true,
+//             actions: [
+//               Builder(
+//                 builder: (context) => IconButton(
+//                   icon: Icon(Icons.save),
+//                   onPressed: () async {
+//                     final status = await Permission.storage.request();
+//                     if (status == PermissionStatus.granted) {
+//                       await _saveScreenShot(cropped);
+//                       Scaffold.of(context).showSnackBar(
+//                         SnackBar(
+//                           content: Text('Saved to gallery.'),
+//                         ),
+//                       );
+//                     }
+//                   },
+//                 ),
+//               ),
+//             ],
+//           ),
+//           body: Center(
+//             child: RawImage(
+//               image: cropped,
+//             ),
+//           ),
+//         ),
+//         fullscreenDialog: true,
+//       ),
+//     );
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final theme = Theme.of(context);
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Crop Demo'),
+//         centerTitle: true,
+//         leading: IconButton(
+//           icon: Icon(FontAwesome.github),
+//           onPressed: () {
+//             launch('https://github.com/xclud/flutter_crop');
+//           },
+//         ),
+//         actions: <Widget>[
+//           IconButton(
+//             onPressed: _cropImage,
+//             tooltip: 'Crop',
+//             icon: Icon(Icons.crop),
+//           )
+//         ],
+//       ),
+//       body: Column(
+//         children: <Widget>[
+//           Expanded(
+//             child: Container(
+//               color: Colors.black,
+//               padding: EdgeInsets.all(8),
+//               child: Crop(
+//                 onChanged: (decomposition) {
+//                   print(
+//                       "Scale : ${decomposition.scale}, Rotation: ${decomposition.rotation}, translation: ${decomposition.translation}");
+//                 },
+//                 controller: controller,
+//                 shape: shape,
+//                 child: Image.asset(
+//                   'images/sample.jpg',
+//                   fit: BoxFit.cover,
+//                 ),
+//                 /* It's very important to set `fit: BoxFit.cover`.
+//                    Do NOT remove this line.
+//                    There are a lot of issues on github repo by people who remove this line and their image is not shown correctly.
+//                 */
+//                 foreground: IgnorePointer(
+//                   child: Container(
+//                     alignment: Alignment.bottomRight,
+//                     child: Text(
+//                       'Foreground Object',
+//                       style: TextStyle(color: Colors.red),
+//                     ),
+//                   ),
+//                 ),
+//                 helper: shape == CropShape.box
+//                     ? Container(
+//                         decoration: BoxDecoration(
+//                           border: Border.all(color: Colors.white, width: 2),
+//                         ),
+//                       )
+//                     : null,
+//               ),
+//             ),
+//           ),
+//           Row(
+//             children: <Widget>[
+//               IconButton(
+//                 icon: Icon(Icons.undo),
+//                 tooltip: 'Undo',
+//                 onPressed: () {
+//                   controller.rotation = 0;
+//                   controller.scale = 1;
+//                   controller.offset = Offset.zero;
+//                   setState(() {
+//                     _rotation = 0;
+//                   });
+//                 },
+//               ),
+//               Expanded(
+//                 child: SliderTheme(
+//                   data: theme.sliderTheme.copyWith(
+//                     trackShape: CenteredRectangularSliderTrackShape(),
+//                   ),
+//                   child: Slider(
+//                     divisions: 361,
+//                     value: _rotation,
+//                     min: -180,
+//                     max: 180,
+//                     label: '$_rotation°',
+//                     onChanged: (n) {
+//                       setState(() {
+//                         _rotation = n.roundToDouble();
+//                         controller.rotation = _rotation;
+//                       });
+//                     },
+//                   ),
+//                 ),
+//               ),
+//               PopupMenuButton<CropShape>(
+//                 icon: Icon(Icons.crop_free),
+//                 itemBuilder: (context) => [
+//                   PopupMenuItem(
+//                     child: Text("Box"),
+//                     value: CropShape.box,
+//                   ),
+//                   PopupMenuItem(
+//                     child: Text("Oval"),
+//                     value: CropShape.oval,
+//                   ),
+//                 ],
+//                 tooltip: 'Crop Shape',
+//                 onSelected: (x) {
+//                   setState(() {
+//                     shape = x;
+//                   });
+//                 },
+//               ),
+//               PopupMenuButton<double>(
+//                 icon: Icon(Icons.aspect_ratio),
+//                 itemBuilder: (context) => [
+//                   PopupMenuItem(
+//                     child: Text("Original"),
+//                     value: 1000 / 667.0,
+//                   ),
+//                   PopupMenuDivider(),
+//                   PopupMenuItem(
+//                     child: Text("16:9"),
+//                     value: 16.0 / 9.0,
+//                   ),
+//                   PopupMenuItem(
+//                     child: Text("4:3"),
+//                     value: 4.0 / 3.0,
+//                   ),
+//                   PopupMenuItem(
+//                     child: Text("1:1"),
+//                     value: 1,
+//                   ),
+//                   PopupMenuItem(
+//                     child: Text("3:4"),
+//                     value: 3.0 / 4.0,
+//                   ),
+//                   PopupMenuItem(
+//                     child: Text("9:16"),
+//                     value: 9.0 / 16.0,
+//                   ),
+//                 ],
+//                 tooltip: 'Aspect Ratio',
+//                 onSelected: (x) {
+//                   controller.aspectRatio = x;
+//                   setState(() {});
+//                 },
+//               ),
+//             ],
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+// Future<dynamic> _saveScreenShot(ui.Image img) async {
+//   var byteData = await img.toByteData(format: ui.ImageByteFormat.png);
+//   var buffer = byteData.buffer.asUint8List();
+//   final result = await ImageGallerySaver.saveImage(buffer);
+//   print(result);
+
+//   return result;
+// }
